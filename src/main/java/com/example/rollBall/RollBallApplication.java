617806a4d907fefba2e02cf68ba7c4bd3150a38b
zabikhanjava <ismail.zabiulla@valtech.com>
2022-01-01 19:50:19 +0530

    getmapping

--- src/main/java/com/example/rollBall/RollBallApplication.java
+++ src/main/java/com/example/rollBall/RollBallApplication.java
@@ -2,9 +2,13 @@ package com.example.rollBall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class RollBallApplication {
+
+	@GetMapping
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RollBallApplication.class, args);
